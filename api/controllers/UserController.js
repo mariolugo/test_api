29f83df09eabcff6649e6bc1ce4f00d5b71b4aa9
Mario Lugo <mariolugo23@gmail.com>
2017-01-11 21:48:01 -0700

    user crud

--- api/controllers/UserController.js
+++ api/controllers/UserController.js
@@ -6,6 +6,69 @@
  */
 
 module.exports = {
-	
+	/**
+     * Add new user
+     * @param {Object} req Request object
+     * @param {Object} res Response object
+     */
+    newUser: (req,res)=>{
+        User 
+            .create(_.omit(req.allParams(), 'id'))
+            .exec((err,user) => {
+                if (err) return res.negotiate(err);
+                if (!user) return res.notFound();
+                User.find()
+                .exec((err, users) => {
+                    res.ok({
+                        'CODE': 'CREATED',
+                        users: users
+                    });
+                });
+            });
+    },
+    /**
+     * delete user
+     * @param {Object} req Request object
+     * @param {Object} res Response object
+     */
+    deleteUser: (req, res) => {
+        User
+            .destroy({
+                id: req.param('id')
+            })
+            .exec((err,user)=>{
+                if (err) return res.negotiate(err);
+                if (user.length == 0) return res.notFound();
+                User.find()
+                .exec((err, users) => {
+                    res.ok({
+                        'CODE': 'DELETED',
+                        users: users
+                    });
+                });
+            });
+    },
+    /**
+     * edit user
+     * @param {Object} req Request object
+     * @param {Object} res Response object
+     */
+    editUser: (req,res) => {
+        User
+            .update({
+                id: req.param('id')
+            },_.omit(req.allParams(), 'id'))
+            .exec((err,user) => {
+                if (err) return res.negotiate(err);
+                if (user.length == 0) return res.notFound();
+                User.find()
+                .exec((err, users) => {
+                    res.ok({
+                        'CODE': 'EDITED',
+                        users: users
+                    });
+                });
+            });
+    }
 };
 
